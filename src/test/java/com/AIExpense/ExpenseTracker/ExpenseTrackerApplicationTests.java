4c8984f4e3a2b357b954c37d70a8238ccd3a261c
Rahul <rahulh1798@gmail.com>
2026-05-29 16:14:40 +0530

    Inital Spring boot setup

---
+++ src/test/java/com/AIExpense/ExpenseTracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AIExpense.ExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
