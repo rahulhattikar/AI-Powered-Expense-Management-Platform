6047dd76e2a741bb77e07e7d38c1662b66adba1b
Rahul <rahulh1798@gmail.com>
2026-05-31 10:57:37 +0530

    feat: day 2 - user, expense, budget entities and database schema

---
+++ src/main/java/com/AIExpense/ExpenseTracker/entity/Budget.java
@@ -0,0 +1,45 @@
+package com.AIExpense.ExpenseTracker.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "budgets")
+public class Budget {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private String category;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal monthlyLimit;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
