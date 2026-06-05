3b8bf6b1109e42d33f267691ad577f2b15b8962a
Rahul <rahulh1798@gmail.com>
2026-06-05 17:44:35 +0530

    Add password encryption support

---
+++ src/main/java/com/AIExpense/ExpenseTracker/auth/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.AIExpense.ExpenseTracker.auth.service;
+
+import com.AIExpense.ExpenseTracker.auth.dto.AuthResponse;
+import com.AIExpense.ExpenseTracker.auth.dto.LoginRequest;
+import com.AIExpense.ExpenseTracker.auth.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthService {
+    AuthResponse register(RegisterRequest request);
+    AuthResponse login(LoginRequest request);
+}
